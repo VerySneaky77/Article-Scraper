e805482c22c21f4b3a78c72c74ca89b4f5b5861c
Carlos Quinto <carlquin77@gmail.com>
2019-06-11 15:41:41 -0700

    Setting up server.js connections and mongoDB connection to heroku's mongolab

--- server.js
+++ server.js
@@ -1,6 +1,30 @@
 const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+// Scraping tools
+const axios = require("axios");
+const cheerio = require("cheerio");
+// Require all models
+const db = require("./models");
+// Set port
 const PORT = process.env.PORT || 8080;
+
+// Initialise app
 const app = express();
+// Configure middleware
+
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Make 'public' a static folder
+app.use(express.static("public"));
+
+// Connect to the Mongo DB
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(MONGODB_URI);
 
 // Set Handlebars.
 const exphbs = require("express-handlebars");
